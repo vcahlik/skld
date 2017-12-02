8b6c671ab8f8f99c05395d75f1be00d68ea4b467
selverob <selverob@6688e65b-4e2e-4002-b0cc-31bcfa2f246f>
2017-12-02 10:08:50 +0000

    Add Hibernate configuration and initialization

--- api/api/src/main/java/cz/cvut/fit/project/skld/api/SKLDAPIConfiguration.java
+++ api/api/src/main/java/cz/cvut/fit/project/skld/api/SKLDAPIConfiguration.java
@@ -3,8 +3,24 @@ package cz.cvut.fit.project.skld.api;
 import io.dropwizard.Configuration;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import org.hibernate.validator.constraints.*;
+
+import javax.validation.Valid;
 import javax.validation.constraints.*;
+import io.dropwizard.db.DataSourceFactory;
+import org.hibernate.validator.constraints.NotEmpty;
 
 public class SKLDAPIConfiguration extends Configuration {
-    // TODO: implement service configuration
+    @Valid
+    @NotNull
+    private DataSourceFactory database = new DataSourceFactory();
+
+    @JsonProperty("database")
+    public DataSourceFactory getDataSourceFactory() {
+        return database;
+    }
+
+    @JsonProperty("database")
+    public void setDataSourceFactory(DataSourceFactory dataSourceFactory) {
+        this.database = dataSourceFactory;
+    }
 }
