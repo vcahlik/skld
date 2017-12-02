229caaf43e205c3dea1984dcbe18642d172470b3
selverob <selverob@6688e65b-4e2e-4002-b0cc-31bcfa2f246f>
2017-12-02 10:08:50 +0000

    Add Hibernate configuration and initialization

--- api/api/src/main/java/cz/cvut/fit/project/skld/api/SKLDAPIApplication.java
+++ api/api/src/main/java/cz/cvut/fit/project/skld/api/SKLDAPIApplication.java
@@ -1,14 +1,25 @@
 package cz.cvut.fit.project.skld.api;
 
+import cz.cvut.fit.project.skld.api.core.User;
 import io.dropwizard.Application;
+import io.dropwizard.db.DataSourceFactory;
 import io.dropwizard.setup.Bootstrap;
 import io.dropwizard.setup.Environment;
+import io.dropwizard.hibernate.HibernateBundle;
 
 public class SKLDAPIApplication extends Application<SKLDAPIConfiguration> {
 
     public static void main(final String[] args) throws Exception {
         new SKLDAPIApplication().run(args);
     }
+
+    private final HibernateBundle<SKLDAPIConfiguration> hibernateBundle =
+            new HibernateBundle<SKLDAPIConfiguration>(User.class) {
+                @Override
+                public DataSourceFactory getDataSourceFactory(SKLDAPIConfiguration configuration) {
+                    return configuration.getDataSourceFactory();
+                }
+            };
 
     @Override
     public String getName() {
@@ -17,7 +28,7 @@ public class SKLDAPIApplication extends Application<SKLDAPIConfiguration> {
 
     @Override
     public void initialize(final Bootstrap<SKLDAPIConfiguration> bootstrap) {
-        // TODO: application initialization
+        bootstrap.addBundle(hibernateBundle);
     }
 
     @Override
