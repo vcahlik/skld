3a269fcf8c86f81483142143f37df954891c6045
selverob <selverob@6688e65b-4e2e-4002-b0cc-31bcfa2f246f>
2017-12-07 15:10:45 +0000

    (3) Code consolidation: Remove the src directory

--- src/client/skld-client/src/main/java/cz/cvut/fit/si1/skld/client/Frame.java
+++
@@ -1,7 +0,0 @@
-package cz.cvut.fit.si1.skld.client;
-
-public abstract class Frame extends Screen implements Wrapper {
-    public Frame(Passable source) {
-        super(source);
-    }
-}
--- src/client/skld-client/src/main/java/cz/cvut/fit/si1/skld/client/util/exceptions/InputErrorException.java
+++
@@ -1,4 +0,0 @@
-package cz.cvut.fit.si1.skld.client.util.exceptions;
-
-public class InputErrorException extends Exception {
-}
