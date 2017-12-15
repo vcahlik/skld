6b549a38743187aac98f08b2e8736ad6086c2723
cahlivoj <cahlivoj@6688e65b-4e2e-4002-b0cc-31bcfa2f246f>
2017-12-15 17:17:39 +0000

    Client gui: soubor s konfiguraci

--- src/gui/src/main/java/cz/cvut/fit/project/skld/gui/resources/Config.java
+++ src/gui/src/main/java/cz/cvut/fit/project/skld/gui/resources/Config.java
@@ -1,5 +1,43 @@
 package cz.cvut.fit.project.skld.gui.resources;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
 public class Config {
-    public static final String SERVER_URL = "http://localhost:8080";
+    private static Config instance;
+
+    private static String configFilePath;
+    private Properties properties;
+
+    protected Config() {
+        properties = new Properties();
+        FileInputStream in = null;
+        try {
+            in = new FileInputStream(configFilePath);
+            properties.load(in);
+            in.close();
+        } catch (IOException e) {
+            // Config file not present or IO error
+            e.printStackTrace();
+            System.exit(1);
+            return;
+        }
+    }
+
+    public static Config getInstance() {
+        if (instance == null) {
+            instance = new Config();
+        }
+        return instance;
+    }
+
+    public String getServerUrl() {
+        return properties.getProperty("serverUrl", "http://localhost:8080");
+    }
+
+    public static void setConfigFilePath(String configFilePath) {
+        Config.configFilePath = configFilePath;
+    }
+
 }
