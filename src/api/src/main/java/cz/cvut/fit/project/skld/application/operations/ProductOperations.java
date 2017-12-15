4b3244c93fd04019697d84d161d990d3eb62138b
selverob <selverob@6688e65b-4e2e-4002-b0cc-31bcfa2f246f>
2017-12-15 21:38:55 +0000

    Add Javadocs for operations package

--- src/api/src/main/java/cz/cvut/fit/project/skld/application/operations/ProductOperations.java
+++ src/api/src/main/java/cz/cvut/fit/project/skld/application/operations/ProductOperations.java
@@ -15,18 +15,32 @@ import org.slf4j.LoggerFactory;
 import java.util.List;
 import java.util.Optional;
 
+/***
+ * Implements operations on products
+ */
 public class ProductOperations {
     private static final Logger LOGGER = LoggerFactory.getLogger(ProductOperations.class);
 
     private final ProductDAO productDAO;
     private final PositionDAO positionDAO;
 
-
+    /***
+     * Construct an instance of the class.
+     * @param pDao DAO used to access product tables
+     * @param posDao DAO used to access product position tables
+     */
     public ProductOperations(ProductDAO pDao, PositionDAO posDao) {
         productDAO = pDao;
         this.positionDAO = posDao;
     }
 
+    /***
+     * Create a new product
+     * @param creator the User that's creating the product and will be set as its creator
+     * @param product the information that should be saved with the product
+     * @return the created product, with its generated fields filled-out
+     * @throws InvalidStateException A product with the given ID already exists in the database.
+     */
     public Product create(User creator, ProductChange product) throws InvalidStateException {
         Optional<Product> existing = productDAO.findById(product.getId());
         if (existing.isPresent()) {
@@ -36,20 +50,41 @@ public class ProductOperations {
         return productDAO.create(p);
     }
 
+    /***
+     * Get a list of all the products that are in the database
+     * @return a list of all the products that are in the database
+     */
     public List<Product> list() {
         return productDAO.findAll();
     }
 
+    /***
+     * Get details of a product with the given ID
+     * @param id the ID of the product
+     * @return the requested product
+     * @throws NotFoundException there is no product with the given ID in the database
+     */
     public Product get(long id) throws NotFoundException {
         return productDAO.findById(id).orElseThrow(new NotFoundExceptionSupplier());
     }
 
+    /***
+     * Change the name of the product with the given ID.
+     * @param change an object representing the change request
+     * @return complete information about the edited product
+     * @throws NotFoundException there is no product with the given ID in the database
+     */
     public Product edit(ProductChange change) throws NotFoundException {
         Product p = productDAO.findById(change.getId()).orElseThrow(new NotFoundExceptionSupplier());
         p.setName(change.getName());
         return p;
     }
 
+    /***
+     * Get a list of positions where the product is currently located in the warehouse
+     * @param id the ID of the product
+     * @returna list of positions where the product is currently located in the warehouse
+     */
     public List<ProductPosition> positionsForProduct(long id) {
         return positionDAO.findForProductId(id);
     }
---
+++ src/api/src/main/java/cz/cvut/fit/project/skld/application/operations/package-info.java
@@ -0,0 +1,4 @@
+/***
+ * Contains classes implementing the business logic of the application.
+ */
+package cz.cvut.fit.project.skld.application.operations;
