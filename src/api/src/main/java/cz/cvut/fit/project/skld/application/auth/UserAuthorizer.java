c8e8dafa53cfedbc0181b482c64af1957f148a89
selverob <selverob@6688e65b-4e2e-4002-b0cc-31bcfa2f246f>
2017-12-15 20:16:39 +0000

    Add Javadocs to auth package.

--- src/api/src/main/java/cz/cvut/fit/project/skld/application/auth/UserAuthorizer.java
+++ src/api/src/main/java/cz/cvut/fit/project/skld/application/auth/UserAuthorizer.java
@@ -3,7 +3,17 @@ package cz.cvut.fit.project.skld.application.auth;
 import cz.cvut.fit.project.skld.application.core.User;
 import io.dropwizard.auth.Authorizer;
 
+/***
+ * Implements checking whether user has the role necessary to perform an action.
+ */
 public class UserAuthorizer implements Authorizer<User> {
+    /***
+     * Checks whether user has the requested role.
+     * @param user the user that should be checked
+     * @param role the role that the user must have to be authorized.
+     *             Currently only "admin" and "user" are supported, controlled by the isAdmin field on User.
+     * @return
+     */
     @Override
     public boolean authorize(User user, String role) {
         if (role.equals("user")) {
