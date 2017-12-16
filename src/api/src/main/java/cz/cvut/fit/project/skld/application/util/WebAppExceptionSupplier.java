61e45e7add4438c19e0db8bea8f7be6422fc9e62
selverob <selverob@6688e65b-4e2e-4002-b0cc-31bcfa2f246f>
2017-12-16 10:13:06 +0000

    Improve WebAppExceptionSupplier documentation

--- src/api/src/main/java/cz/cvut/fit/project/skld/application/util/WebAppExceptionSupplier.java
+++ src/api/src/main/java/cz/cvut/fit/project/skld/application/util/WebAppExceptionSupplier.java
@@ -11,11 +11,18 @@ public class WebAppExceptionSupplier implements Supplier<WebApplicationException
     private final String message;
     private final Response.Status status;
 
+    /***
+     * Construct a new supplier which will return WebApplicationException with the given message and status when calling
+     * its get() method.
+     */
     public WebAppExceptionSupplier(String message, Response.Status status) {
         this.message = message;
         this.status = status;
     }
 
+    /***
+     * Get the exception with the parameters specified in the constructor.
+     */
     public WebApplicationException get() {
         return new WebApplicationException(message, status);
     }
