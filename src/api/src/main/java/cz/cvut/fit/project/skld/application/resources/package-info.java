9dbb303b89da2d7cc4962afb2c93dc3f03f4ce6e
selverob <selverob@6688e65b-4e2e-4002-b0cc-31bcfa2f246f>
2017-12-21 14:10:03 +0000

    Add at least basic documentation to resources package

--- src/api/src/main/java/cz/cvut/fit/project/skld/application/resources/package-info.java
+++ src/api/src/main/java/cz/cvut/fit/project/skld/application/resources/package-info.java
@@ -1,4 +1,9 @@
 /***
  * Implements REST endpoints for the application.
+ *
+ * Please note that the documentation for these classes is quite short. These are not supposed to be used directly by
+ * users but are mostly automatically constructed and called by Dropwizard/Jersey. A lot of functionality is also
+ * hidden in the annotations. Please read at least the introduction to Dropwizard development to get a feel of what
+ * this package is doing and how.
  */
 package cz.cvut.fit.project.skld.application.resources;
